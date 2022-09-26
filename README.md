a3a846614ca058b8711c57c42d0aa2f85ac0ce64
wdy <18408217155@163.com>
2022-09-26 14:16:11 +0800

    feature(all): support eip1559 tx

--- README.md
+++ README.md
@@ -47,6 +47,28 @@ $transaction->getRaw ($privateKey, $chainId);
 // f86d048503f5476a0083027f4b941a8c8adfbe1c59e8b58cc0d515f07b7225f51c72882a45907d1bef7c008025a0db4efcc22a7d9b2cab180ce37f81959412594798cb9af7c419abb6323763cdd5a0631a0c47d27e5b6e3906a419de2d732e290b73ead4172d8598ce4799c13bda69
 ```
 
+Create a EIP1559 transaction
+
+```php
+use kornrunner\Ethereum\EIP1559Transaction;
+
+$nonce = '02';
+$maxPriorityFeePerGas = 'b2d05e00';
+$maxFeePerGas = '6fc23ac00';
+$gasLimit = '5208';
+$to = '1a8c8adfbe1c59e8b58cc0d515f07b7225f51c72';
+$value = '29a2241af62c0000';
+$data = '';
+$chainId = 1;
+
+$privateKey = 'b2f2698dd7343fa5afc96626dee139cb92e58e5d04e855f4c712727bf198e898';
+
+$transaction = new EIP1559Transaction($nonce, $maxPriorityFeePerGas, $maxFeePerGas, $gasLimit, $to, $value, $data);
+
+$rawTx = $transaction->getRaw($privateKey, $chainId);
+// 02f873010284b2d05e008506fc23ac00825208941a8c8adfbe1c59e8b58cc0d515f07b7225f51c728829a2241af62c000080c080a0dd32dc794af9a9085d6772c40656fc156a577570c6fd32f2a2d4126673373919a066cf1859672a9e6fdbb00ebd230bcfec66cac1c99f5c83598ecae6025d0e91f4
+```
+
 ## Crypto
 
 [![Ethereum](https://user-images.githubusercontent.com/725986/61891022-0d0c7f00-af09-11e9-829f-096c039bbbfa.png) 0x9c7b7a00972121fb843af7af74526d7eb585b171][Ethereum]
--- test/TransactionTest.php
+++ test/TransactionTest.php
@@ -2,6 +2,7 @@
 
 namespace kornrunner;
 
+use kornrunner\Ethereum\EIP1559Transaction;
 use kornrunner\Ethereum\Transaction;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
@@ -67,4 +68,28 @@ class TransactionTest extends TestCase {
         $transaction->getRaw('');
     }
 
+    /**
+     * @dataProvider getEIP1559Raw
+     */
+    public function testGetEIP1559Raw($expect, $privateKey, $chainId, $nonce, $maxPriorityFeePerGas, $maxFeePerGas, $gasLimit, $to, $value, $data) {
+        $transaction = new EIP1559Transaction($nonce, $maxPriorityFeePerGas, $maxFeePerGas, $gasLimit, $to, $value, $data);
+        $this->assertSame($expect, $transaction->getRaw($privateKey, $chainId));
+    }
+
+    public static function getEIP1559Raw(): array {
+        return [
+            [
+                '02f873010284b2d05e008506fc23ac00825208941a8c8adfbe1c59e8b58cc0d515f07b7225f51c728829a2241af62c000080c080a0dd32dc794af9a9085d6772c40656fc156a577570c6fd32f2a2d4126673373919a066cf1859672a9e6fdbb00ebd230bcfec66cac1c99f5c83598ecae6025d0e91f4',
+                'b2f2698dd7343fa5afc96626dee139cb92e58e5d04e855f4c712727bf198e898', 1, '02', 'b2d05e00', '6fc23ac00', '5208', '1a8c8adfbe1c59e8b58cc0d515f07b7225f51c72', '29a2241af62c0000', ''
+            ],
+            [
+                '02f873380284b2d05e008506fc23ac00825208941a8c8adfbe1c59e8b58cc0d515f07b7225f51c728829a2241af62c000080c080a0b8ec74b95d0d8ebe4b4c7e9d5a8af16e06a7d636d20b2c632633a2abe3a0f5cca005971b1f3ec9b498d8d7b17b312477d0b1fd2232ca4b82e9c34fc43d33e5f196',
+                'b2f2698dd7343fa5afc96626dee139cb92e58e5d04e855f4c712727bf198e898', 56, '02', 'b2d05e00', '6fc23ac00', '5208', '1a8c8adfbe1c59e8b58cc0d515f07b7225f51c72', '29a2241af62c0000', ''
+            ],
+            [
+                '02f877380284b2d05e008506fc23ac00825208941a8c8adfbe1c59e8b58cc0d515f07b7225f51c728829a2241af62c00008401232213c001a05bc0caa25dd8e23adf3f79f8dbe1237e0f22980a3b15f00bbe9f5a19dd23a70ca002e9a353c9b155b0e7e796b5f966bc6dd53deafea9990398a069d93278e50644',
+                'b2f2698dd7343fa5afc96626dee139cb92e58e5d04e855f4c712727bf198e898', 56, '02', 'b2d05e00', '6fc23ac00', '5208', '1a8c8adfbe1c59e8b58cc0d515f07b7225f51c72', '29a2241af62c0000', '1232213'
+            ]
+        ];
+    }
 }
